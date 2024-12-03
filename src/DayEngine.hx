4f6d6cd9d94bf772f593758f3f8c38f5a5ced5c5
Neill Robson <neill.robson@pendo.io>
2024-12-03 15:15:07 -0500

    Add testing infra, and day 1 p 1 solution

--- src/DayEngine.hx
+++ src/DayEngine.hx
@@ -1,12 +1,38 @@
 import haxe.Timer;
 
+typedef TestData = {
+    var data:String;
+    var expected:Array<Dynamic>;
+}
+
 abstract class DayEngine {
     abstract function problem1(data:String):Dynamic;
     abstract function problem2(data:String):Dynamic;
 
-    public function new(data:String) {
-        for (problem in [problem1, problem2]) {
+    public function new(data:String, ?tests:Array<TestData>) {
+        for (x => problem in [problem1, problem2]) {
             Timer.measure(() -> {
+                Sys.print('Problem ${x + 1}: ');
+
+                if (tests != null) {
+                    for (test in tests) {
+                        if (test.expected.length <= x || test.expected[x] == null) {
+                            Sys.print("❓");
+                            continue;
+                        }
+
+                        var result = problem(test.data);
+
+                        if (result == test.expected[x]) {
+                            Sys.print("✅");
+                        } else {
+                            Sys.print("❌");
+                        }
+                    }
+                }
+
+                Sys.println("");
+
                 var result = problem(data);
 
                 Sys.println(result == null ? "No result" : result);
--- src/y2024/Day1.hx
+++ src/y2024/Day1.hx
@@ -1,12 +1,46 @@
 package y2024;
 
+import DayEngine.TestData;
+
+var testData = '3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+';
+
 class Day1 extends DayEngine {
     public static function make(data:String) {
-        new Day1(data);
+        var tests:Array<TestData> = [
+            { data: testData, expected: [11, null] }
+        ];
+
+        new Day1(data, tests);
     }
 
     function problem1(data:String):Dynamic {
-        return data.split("\n")[0];
+        var first:Array<Int> = [];
+        var second:Array<Int> = [];
+
+        for (line in data.split("\n")) {
+            var parts = line.split("   ");
+            if (parts.length != 2) {
+                continue;
+            }
+            first.push(Std.parseInt(parts[0]));
+            second.push(Std.parseInt(parts[1]));
+        }
+
+        first.sort((a, b) -> a - b);
+        second.sort((a, b) -> a - b);
+
+        var result = 0;
+        for (i in 0...first.length) {
+            result += Math.floor(Math.abs(first[i] - second[i]));
+        }
+
+        return result;
     }
 
     function problem2(data:String):Dynamic {
