002a1329f0e13ab02908aab84bdfa58435f366fd
Neill Robson <neill.robson@pendo.io>
2024-12-06 13:43:33 -0500

    day 3 p 2

--- src/y2024/Day3.hx
+++ src/y2024/Day3.hx
@@ -22,9 +22,14 @@ function postProcess(data:Array<String>):Int {
 class Day3 extends DayEngine {
     public static function make(data:String) {
         var tests:Array<TestData> = [
-            { data: testData, expected: [161, 48] },
-            { data: "mul(2,4),mul(5,5),mul(11,8),mul(8,5)", expected: [161, null] },
-            { data: "mumul(2,4),mul(5,5),mul(11,8),mul(8,5)", expected: [161, null] },
+            { data: testData, expected: [161, 161] },
+            { data: "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", expected: [161, 48] },
+            { data: "mul(2,4),mul(5,5),mul(11,8),mul(8,5)", expected: [161, 161] },
+            { data: "mumul(2,4),mul(5,5),mul(11,8),mul(8,5)", expected: [161, 161] },
+            { data: "mudon't()l(8,5)", expected: [0, 0] },
+            { data: "mudo()l(8,5)", expected: [0, 0] },
+            { data: "mul(8,5)don't()do()mul(8,5)", expected: [80, 80] },
+            { data: "mul(8,5)don't()do(')mul(8,5)", expected: [80, 40] },
         ];
 
         new Day3(data, tests);
@@ -65,7 +70,59 @@ class Day3 extends DayEngine {
     }
 
     function problem2(data:String):Dynamic {
-        return null;
+        var charArr = ['', 'm', 'u', 'l', '(', '0123456789', ',', ')', 'd', 'o', 'n', "'", 't'];
+
+        var states:Array<Array<Array<Int>>> = [];
+
+        for (i in 0...19) {
+            var row = [];
+            for (j in 0...charArr.length) {
+                if (i < 15) {
+                    // Allow for "mul" and "don't"
+                    if (j == 1) row.push([1, 1]);
+                    else if (j == 8) row.push([9, 0]);
+                    else if (i == 8) row.push([0, 3]);
+                    else row.push([0, 0]);
+                } else {
+                    // Allow for "do"
+                    if (j == 8) row.push([16, 0]);
+                    else row.push([15, 0]);
+                }
+            }
+            states.push(row);
+        }
+
+        states[0][1] = [1, 1]; // m
+        states[1][2] = [2, 0]; // u
+        states[2][3] = [3, 0]; // l
+        states[3][4] = [4, 0]; // (
+        states[4][5] = [5, 0]; // 0-9
+        states[5][5] = [5, 0]; // 0-9+
+        states[5][6] = [6, 0]; // ,
+        states[6][5] = [7, 0]; // 0-9
+        states[7][5] = [7, 0]; // 0-9+
+        states[7][7] = [8, 0]; // )
+
+        states[8][1] = [1, 2]; // received another "m"
+        states[8][8] = [9, 3]; // d
+
+        states[0][8] = [9, 0]; //    d
+        states[9][9] = [10, 0]; //  o
+        states[10][10] = [11, 0]; // n
+        states[11][11] = [12, 0]; // '
+        states[12][12] = [13, 0]; // t
+        states[13][4] = [14, 0]; // (
+        states[14][7] = [15, 0]; // )
+
+        states[15][8] = [16, 0]; //  d
+        states[16][9] = [17, 0]; //  o
+        states[17][4] = [18, 0]; // (
+        states[18][7] = [0, 0]; //  )
+
+        var fsm = new FSM(states, charArr);
+        var res = fsm.run(data);
+
+        return postProcess(res);
     }
 }
 
@@ -102,8 +159,8 @@ class FSM {
 
         var out = [];
 
-        for (i in 0...data.length) {
-            var c = charMap[data.charAt(i)];
+        for (i in 0...data.length + 1) {
+            var c = i == data.length ? 0 : charMap[data.charAt(i)];
             if (c == null) c = 0;
 
             var state = states[r][c];
@@ -122,10 +179,6 @@ class FSM {
                 case 6:
                     return out;
             }
-        }
-
-        if (j >= 0) {
-            out.push(data.substring(j));
         }
 
         return out;
