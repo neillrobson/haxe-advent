d8b5a7f35e8cf9284050086193fd3ee2c0a4aa06
Neill Robson <neill.robson@pendo.io>
2024-12-06 09:19:18 -0500

    Day 3 scaffold

---
+++ src/y2024/Day3.hx
@@ -0,0 +1,15 @@
+package y2024;
+
+class Day3 extends DayEngine {
+    public static function make(data:String) {
+        new Day3(data);
+    }
+
+    function problem1(data:String):Dynamic {
+        return null;
+    }
+
+    function problem2(data:String):Dynamic {
+        return null;
+    }
+}
