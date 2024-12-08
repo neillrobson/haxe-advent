3711575c311f958dac0d3b04286e1aaebb953ad8
Neill Robson <neill@neillrobson.com>
2024-12-08 14:50:39 -0500

    Day 5 scaffold

---
+++ src/y2024/Day5.hx
@@ -0,0 +1,51 @@
+package y2024;
+
+import DayEngine.TestData;
+
+var testData = '47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+';
+
+class Day5 extends DayEngine {
+    public static function make(data:String) {
+        var tests:Array<TestData> = [
+            { data: testData, expected: [143, null] }
+        ];
+
+        new Day5(data, tests);
+    }
+
+    function problem1(data:String):Dynamic {
+        return null;
+    }
+
+    function problem2(data:String):Dynamic {
+        return null;
+    }
+}
