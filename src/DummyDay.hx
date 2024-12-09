8e9153647fd3a5731a2e6a951d13bcf62d66b17b
Neill Robson <neill.robson@pendo.io>
2024-12-09 16:59:09 -0500

    Dummy day

---
+++ src/DummyDay.hx
@@ -0,0 +1,21 @@
+import DayEngine.TestData;
+
+var testData = "";
+
+class DayX extends DayEngine {
+    public static function make(data:String) {
+        var tests:Array<TestData> = [
+            { data: testData, expected: [] }
+        ];
+
+        new DayX(data, tests, false);
+    }
+
+    function problem1(data:String):Dynamic {
+        return null;
+    }
+
+    function problem2(data:String):Dynamic {
+        return null;
+    }
+}
