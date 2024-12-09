ee2a7a972370ff1a19dae57635e0924ba2f11dc5
Neill Robson <neill@neillrobson.com>
2024-12-08 19:39:41 -0500

    Day 6 p1

--- src/y2024/Day6.hx
+++ src/y2024/Day6.hx
@@ -2,15 +2,123 @@ package y2024;
 
 import DayEngine.TestData;
 
+using StringTools;
+
+var testData = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+";
+
+enum Dir {
+    UP;
+    RIGHT;
+    DOWN;
+    LEFT;
+}
+
 class Day6 extends DayEngine {
     public static function make(data:String) {
-        var tests:Array<TestData> = [];
+        var tests:Array<TestData> = [
+            { data: testData, expected: [41, null] }
+        ];
 
         new Day6(data, tests);
     }
 
     function problem1(data:String):Dynamic {
-        return null;
+        var lines = data.split('\n').map(s -> s.trim()).filter(s -> s.length != 0);
+        var map:Array<Array<Int>> = [];
+        var x:Int = -1, y:Int = -1;
+
+        for (i => line in lines) {
+            var lineArr = [];
+
+            for (j in 0...line.length) {
+                var c = line.charAt(j);
+                lineArr.push(c == "#" ? -1 : 0);
+                if (c == "^") {
+                    x = j;
+                    y = i;
+                    lineArr[j] = 1;
+                }
+            }
+
+            map.push(lineArr);
+        }
+
+        if (x < 0 || y < 0) {
+            Sys.println("Initial location not found");
+            return null;
+        }
+
+        var count = 1;
+        var dir = Dir.UP;
+
+        while (true) {
+            switch (dir) {
+                case Dir.UP:
+                    var newY = y - 1;
+                    if (newY < 0 || newY >= map.length)
+                        break;
+                    if (map[newY][x] < 0)
+                        dir = Dir.RIGHT;
+                    else {
+                        y = newY;
+                        if (map[y][x] == 0) {
+                            map[y][x] = 1;
+                            ++count;
+                        }
+                    }
+                case Dir.RIGHT:
+                    var newX = x + 1;
+                    if (newX < 0 || newX >= map[0].length)
+                        break;
+                    if (map[y][newX] < 0)
+                        dir = Dir.DOWN;
+                    else {
+                        x = newX;
+                        if (map[y][x] == 0) {
+                            map[y][x] = 1;
+                            ++count;
+                        }
+                    }
+                case Dir.DOWN:
+                    var newY = y + 1;
+                    if (newY < 0 || newY >= map.length)
+                        break;
+                    if (map[newY][x] < 0)
+                        dir = Dir.LEFT;
+                    else {
+                        y = newY;
+                        if (map[y][x] == 0) {
+                            map[y][x] = 1;
+                            ++count;
+                        }
+                    }
+                case Dir.LEFT:
+                    var newX = x - 1;
+                    if (newX < 0 || newX >= map[0].length)
+                        break;
+                    if (map[y][newX] < 0)
+                        dir = Dir.UP;
+                    else {
+                        x = newX;
+                        if (map[y][x] == 0) {
+                            map[y][x] = 1;
+                            ++count;
+                        }
+                    }
+            }
+        }
+
+        return count;
     }
 
     function problem2(data:String):Dynamic {
