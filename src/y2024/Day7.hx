707c401f38bea84e622ad63fe2ff53e2427a38eb
Neill Robson <neill.robson@pendo.io>
2024-12-09 16:45:33 -0500

    Day 7 done, part 2 at 7s

--- src/y2024/Day7.hx
+++ src/y2024/Day7.hx
@@ -20,7 +20,7 @@ var testData = "190: 10 19
 class Day7 extends DayEngine {
     public static function make(data:String) {
         var tests:Array<TestData> = [
-            { data: testData, expected: ["3749", null] }
+            { data: testData, expected: ["3749", "11387"] }
         ];
 
         new Day7(data, tests, true);
@@ -45,7 +45,23 @@ class Day7 extends DayEngine {
     }
 
     function problem2(data:String):Dynamic {
-        return null;
+        var result = data
+            .split("\n")
+            .map(s -> s.trim())
+            .filter(s -> s.length > 0)
+            .fold((s, sum) -> {
+                var data = s.split(':');
+                var goal = Int64.parseString(data[0]);
+                var nums = data[1].trim().split(' ').map(s -> Std.parseInt(s) ?? 0);
+
+                if (checkLineConcat(goal, nums)) sum += goal;
+
+                // Sys.println(sum.toStr());
+
+                return sum;
+            }, 0);
+
+        return result.toStr();
     }
 }
 
@@ -60,4 +76,29 @@ function checkRecurse(goal:Int64, nums:Array<Int64>, i:Int, op:Bool, acc:Int64) 
     else acc += nums[i];
 
     return checkRecurse(goal, nums, i+1, false, acc) || checkRecurse(goal, nums, i+1, true, acc);
+}
+
+function checkLineConcat(goal:Int64, nums:Array<Int>):Bool {
+    return checkRecurseConcat(goal, nums, 1, "+", nums[0]) ||
+        checkRecurseConcat(goal, nums, 1, "*", nums[0]) ||
+        checkRecurseConcat(goal, nums, 1, "||", nums[0]);
+}
+
+var l10 = Math.log(10);
+
+function checkRecurseConcat(goal:Int64, nums:Array<Int>, i:Int, op:String, acc:Int64) {
+    if (i == nums.length) return goal == acc;
+
+    if (op == "*") acc *= nums[i];
+    else if (op == "+") acc += nums[i];
+    else {
+        var shift = Std.int(Math.pow(10, 1 + Math.floor(Math.log(nums[i]) / l10)));
+        acc = (acc * shift) + nums[i];
+    }
+
+    if (acc > goal) return false;
+
+    return checkRecurseConcat(goal, nums, i+1, "+", acc) ||
+        checkRecurseConcat(goal, nums, i+1, "*", acc) ||
+        checkRecurseConcat(goal, nums, i+1, "||", acc);
 }
