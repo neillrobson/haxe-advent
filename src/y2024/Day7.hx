75c2759034ef2eac6da8758a8e466187943a921c
Neill Robson <neill.robson@pendo.io>
2024-12-09 16:16:22 -0500

    Int64 wow

--- src/y2024/Day7.hx
+++ src/y2024/Day7.hx
@@ -4,8 +4,7 @@ import DayEngine.TestData;
 
 using Lambda;
 using StringTools;
-
-// 1041118519 too low
+using haxe.Int64;
 
 var testData = "190: 10 19
 3267: 81 40 27
@@ -21,26 +20,28 @@ var testData = "190: 10 19
 class Day7 extends DayEngine {
     public static function make(data:String) {
         var tests:Array<TestData> = [
-            { data: testData, expected: [3749, null] }
+            { data: testData, expected: ["3749", null] }
         ];
 
         new Day7(data, tests, true);
     }
 
     function problem1(data:String):Dynamic {
-        return data
+        var result = data
             .split("\n")
             .map(s -> s.trim())
             .filter(s -> s.length > 0)
             .fold((s, sum) -> {
                 var data = s.split(':');
-                var goal = Std.parseInt(data[0]);
-                var nums = data[1].trim().split(' ').map(n -> Std.parseInt(n) ?? 0);
+                var goal = Int64.parseString(data[0]);
+                var nums = data[1].trim().split(' ').map(Int64.parseString);
 
                 if (checkLine(goal, nums)) sum += goal;
 
                 return sum;
             }, 0);
+
+        return result.toStr();
     }
 
     function problem2(data:String):Dynamic {
@@ -48,11 +49,11 @@ class Day7 extends DayEngine {
     }
 }
 
-function checkLine(goal:Int, nums:Array<Int>):Bool {
+function checkLine(goal:Int64, nums:Array<Int64>):Bool {
     return checkRecurse(goal, nums, 1, false, nums[0]) || checkRecurse(goal, nums, 1, true, nums[0]);
 }
 
-function checkRecurse(goal:Int, nums:Array<Int>, i:Int, op:Bool, acc:Int) {
+function checkRecurse(goal:Int64, nums:Array<Int64>, i:Int, op:Bool, acc:Int64) {
     if (i == nums.length) return goal == acc;
 
     if (op) acc *= nums[i];
