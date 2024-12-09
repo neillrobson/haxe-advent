3f74e5fd028af4f315cc430fda415f9afb108725
Neill Robson <neill@neillrobson.com>
2024-12-08 19:02:33 -0500

    Day 6 scaffold

---
+++ src/y2024/Day6.hx
@@ -0,0 +1,19 @@
+package y2024;
+
+import DayEngine.TestData;
+
+class Day6 extends DayEngine {
+    public static function make(data:String) {
+        var tests:Array<TestData> = [];
+
+        new Day6(data, tests);
+    }
+
+    function problem1(data:String):Dynamic {
+        return null;
+    }
+
+    function problem2(data:String):Dynamic {
+        return null;
+    }
+}
