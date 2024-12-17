0847856c0ed7247be3fa17c8458e7a0cc4324976
Neill Robson <neill.robson@pendo.io>
2024-12-17 09:55:36 -0500

    day 10 part 2

--- src/y2024/Day10.hx
+++ src/y2024/Day10.hx
@@ -19,7 +19,7 @@ var testData = "
 
 class Day10 extends DayEngine {
     public static function make(data:String) {
-        var tests:Array<TestData> = [{data: testData, expected: [36]}];
+        var tests:Array<TestData> = [{data: testData, expected: [36, 81]}];
 
         new Day10(data, tests, true);
     }
@@ -44,7 +44,22 @@ class Day10 extends DayEngine {
     }
 
     function problem2(data:String):Dynamic {
-        return null;
+        var map:Array<Array<Int>> = data.split('\n').map(s -> s.trim()).filter(s -> s.length > 0).map(s -> {
+            var arr = [];
+            for (i in 0...s.length)
+                arr.push(Std.parseInt(s.charAt(i)));
+
+            return arr;
+        });
+
+        var sum:Int = 0;
+
+        for (i in 0...map.length)
+            for (j in 0...map[0].length)
+                if (map[i][j] == 0)
+                    sum += countUniquePaths(map, i, j);
+
+        return sum;
     }
 }
 
@@ -76,4 +91,25 @@ function scorePath(data:Array<Array<Int>>, i:Int, j:Int) {
     }
 
     return locs.length;
+}
+
+function countUniquePaths(data:Array<Array<Int>>, i:Int, j:Int) {
+    var c = data[i][j];
+
+    if (c == 9)
+        return 1;
+
+    var sum = 0;
+    var n = c + 1;
+
+    if (j - 1 >= 0 && data[i][j - 1] == n)
+        sum += countUniquePaths(data, i, j - 1);
+    if (i - 1 >= 0 && data[i - 1][j] == n)
+        sum += countUniquePaths(data, i - 1, j);
+    if (j + 1 < data[0].length && data[i][j + 1] == n)
+        sum += countUniquePaths(data, i, j + 1);
+    if (i + 1 < data.length && data[i + 1][j] == n)
+        sum += countUniquePaths(data, i + 1, j);
+
+    return sum;
 }
