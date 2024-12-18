69cd667309c21fd8b5123255c17039c0db2fe3dc
Neill Robson <neill.robson@pendo.io>
2024-12-18 14:20:37 -0500

    Iterator for int64map

--- src/util/Int64Map.hx
+++ src/util/Int64Map.hx
@@ -47,4 +47,59 @@ abstract Int64Map<T>(Map<Int, Map<Int, T>>) {
             false;
         }
     }
+
+    public inline function keyValueIterator():KeyValueIterator<Int64, T> {
+        return new Int64MapIterator(this.keyValueIterator());
+    }
+}
+
+class DummyIterator {
+    public inline function new() {}
+
+    public inline function hasNext() {
+        return false;
+    }
+
+    public inline function next() {
+        return null;
+    }
+}
+
+class Int64MapIterator<T> {
+    var outer:KeyValueIterator<Int, Map<Int, T>>;
+    var inner:KeyValueIterator<Int, T> = new DummyIterator();
+    var low:Null<Int> = null;
+
+    public inline function new(outer:KeyValueIterator<Int, Map<Int, T>>) {
+        this.outer = outer;
+    }
+
+    public inline function hasNext():Bool {
+        while (!inner.hasNext() && outer.hasNext()) {
+            var kv = outer.next();
+            low = kv.key;
+            inner = kv.value.keyValueIterator();
+        }
+
+        return inner.hasNext();
+    }
+
+    public inline function next():{key:Int64, value:T} {
+        while (!inner.hasNext() && outer.hasNext()) {
+            var kv = outer.next();
+            low = kv.key;
+            inner = kv.value.keyValueIterator();
+        }
+
+        if (!inner.hasNext())
+            return null;
+
+        var kv = inner.next();
+        var high = kv.key;
+
+        return {
+            key: Int64.make(high, low),
+            value: kv.value
+        }
+    }
 }
--- src/y2024/Day11.hx
+++ src/y2024/Day11.hx
@@ -14,24 +14,23 @@ class Day11 extends DayEngine {
     public static function make(data:String) {
         var tests:Array<TestData> = [{data: testData, expected: ["55312"]}];
 
-        // sure(digitCount(0) == 1);
-        // sure(digitCount(1) == 1);
-        // sure(digitCount(9) == 1);
-        // sure(digitCount(10) == 2);
-        // sure(digitCount(99) == 2);
-        // sure(digitCount(100) == 3);
-
-        // sure(mag(0) == 1);
+        sure(mag(0) == 1);
         sure(mag(1) == 1);
         sure(mag(9) == 1);
         sure(mag(10) == 2);
         sure(mag(99) == 2);
         sure(mag(100) == 3);
+        sure(mag(100000000000000000i64) == 18);
+        sure(mag(1000000000000000000i64) == -1);
 
-        for (i in powersOfTen)
-            Sys.println((i - 1).toStr());
+        var testMap = new Int64Map<Int>();
+        testMap.set(123, 45);
+        testMap.set(1000000000000000i64, 78);
 
-        new Day11(data, tests, true);
+        for (k => v in testMap)
+            Sys.println('${k.toStr()}: $v');
+
+        new Day11(data, tests, false);
     }
 
     function problem1(data:String):Dynamic {
@@ -52,41 +51,30 @@ class Day11 extends DayEngine {
 }
 
 var powersOfTen = [
-    0x1i64,
-    0xAi64,
-    0x64i64,
-    0x3E8i64,
-    0x2710i64,
-    0x186A0i64,
-    0xF4240i64,
-    0x989680i64,
-    0x5F5E100i64,
-    0x3B9ACA00i64,
-    0x2540BE400i64,
-    0x174876E800i64,
-    0xE8D4A51000i64,
-    0x9184E72A000i64,
-    0x5AF3107A4000i64,
-    0x38D7EA4C68000i64,
-    0x2386F26FC10000i64,
-    0x16345785D8A0000i64,
-    0xDE0B6B3A7640000i64,
-    0x8AC7230489E80000i64,
-    0xFFFFFFFFFFFFFFFFi64
+    0i64,
+    10i64,
+    100i64,
+    1000i64,
+    10000i64,
+    100000i64,
+    1000000i64,
+    10000000i64,
+    100000000i64,
+    1000000000i64,
+    10000000000i64,
+    100000000000i64,
+    1000000000000i64,
+    10000000000000i64,
+    100000000000000i64,
+    1000000000000000i64,
+    10000000000000000i64,
+    100000000000000000i64,
+    1000000000000000000i64
 ];
 
 inline function mag(n:Int64):Int {
     return powersOfTen.findIndex((i) -> i > n);
 }
-
-// var ln10 = Math.log(10);
-// inline function digitCount(n:Int64):Int {
-//     try {
-//         var i = n.toInt();
-//         return 1 + Math.floor(Math.max(Math.log(i), 0) / ln10);
-//     } catch (e)
-//         return n.toStr().length;
-// }
 
 function stoneCount(n:Int64, i:Int):Int64 {
     static var cache:Int64Map<{a:Int64, b:Int64}> = new Int64Map();
@@ -125,11 +113,6 @@ function stoneCount(n:Int64, i:Int):Int64 {
             ab = {a: a, b: b};
             cache.set(n, ab);
         }
-
-        // var s = Std.string(n);
-        // var d = s.length >> 1;
-        // var a = Std.parseInt(s.substr(0, d));
-        // var b = Std.parseInt(s.substr(d));
 
         return stoneCount(ab.a, i) + stoneCount(ab.b, i);
     } else
